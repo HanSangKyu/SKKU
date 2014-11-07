6e6fecf7553396edccf4c29d08670fa02c4d50f6
조은현 <nyu531@gmail.com>
2014-11-07 18:48:27 +0900

    초기 프로젝트입니다.
    layout은 activity_main.xml -> activity_monitor.xml -> activity_inform.xml 순으로 진행이 되고
    
    각각 MainActivity.java, MonitorActivity.java, InformActivity.java가 각 레이아웃을 담당하고 있습니다
    
    아이비콘에서 얻을 수 있는 정보는 크게 UUID, major, minor, proximity, accuracy, rssi, txPower 정도를 얻을 수 있는데
    
    UUID는 아이비콘을 소유하는 회사의 키값, major는 아이비콘 그룹ID, minor는 아이비콘 각각의 ID라고 볼 수 있습니다.
    
    이 3가지가 아이비콘을 구분할 수 있는 정보가 될 것이고, 현재 리스트뷰에는 major+minor를 키값으로 표기했고 이 키값을 InformActivity에 넘겨줍니다
    
    proximity는 Integer값으로 대략의 거리를 나타내고 accuracy가 double값으로 meter단위의 거리를 나타내는데 정확하지 않은 값들이라고 api(com.radiusnetworks.ibeacon/IBeacon.java)에 주석으로 명시되어 있습니다. 하지만 테스트 했을 때 멀어지면 늘어나고 가까워지면 줄어드는 정도의 정확성은 가지고 있는 것 같습니다.
    
    rssi와 txPower는 어플에서 중요하지 않은 값이므로 생략합니다.
    
    1학년들이 수정해야 할 건 activity_inform.xml과 InformActivity.java입니다. activity_inform.xml은 리스트뷰의 아이템을 클릭했을 때 성균관대 건물의 자세한 정보를 나타내야하는데 현재는 key값을 받아서 key값만 나타내주고 있습니다.
    
    우선 건물에 대한 정보를 InformActivity에 저장하고 activity_inform.xml의 형태를 수정해주세요
    
    이상한거 있으면 연락해요

---
+++ skkuiBeacon/src/com/ion/skkuiBeacon/MonitoringActivity.java
@@ -0,0 +1,186 @@
+package com.ion.skkuiBeacon;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.RemoteException;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.ibeacon.R;
+import com.radiusnetworks.ibeacon.IBeacon;
+import com.radiusnetworks.ibeacon.IBeaconConsumer;
+import com.radiusnetworks.ibeacon.IBeaconManager;
+import com.radiusnetworks.ibeacon.MonitorNotifier;
+import com.radiusnetworks.ibeacon.RangeNotifier;
+import com.radiusnetworks.ibeacon.Region;
+
+public class MonitoringActivity extends Activity implements IBeaconConsumer {
+	protected static final String TAG = "MonitoringActivity";
+
+	private ListView list = null;
+	private BeaconAdapter adapter = null;
+	private ArrayList<IBeacon> arrayL = new ArrayList<IBeacon>();
+	private LayoutInflater inflater;
+
+	private BeaconServiceUtility beaconUtill = null;
+	private IBeaconManager iBeaconManager = IBeaconManager.getInstanceForApplication(this);
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_monitor);
+		beaconUtill = new BeaconServiceUtility(this);
+		list = (ListView) findViewById(R.id.list);
+		adapter = new BeaconAdapter();
+		list.setAdapter(adapter);
+		inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+		list.setOnItemClickListener(new OnItemClickListener() {	 
+			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+				Intent newintent = new Intent(MonitoringActivity.this, InformActivity.class);
+				newintent.putExtra("key", arrayL.get(position).getMajor() + "-" + arrayL.get(position).getMinor());
+				startActivity(newintent);
+			}
+		} );
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+	}
+
+	@Override
+	protected void onStart() {
+		super.onStart();
+		beaconUtill.onStart(iBeaconManager, this);
+	}
+
+	@Override
+	protected void onStop() {
+		beaconUtill.onStop(iBeaconManager, this);
+		super.onStop();
+	}
+
+	@Override
+	public void onIBeaconServiceConnect() {
+
+		iBeaconManager.setRangeNotifier(new RangeNotifier() {
+			@Override
+			public void didRangeBeaconsInRegion(Collection<IBeacon> iBeacons, Region region) {
+
+				arrayL.clear();
+				arrayL.addAll((ArrayList<IBeacon>) iBeacons);
+				adapter.notifyDataSetChanged();
+			}
+
+		});
+
+		iBeaconManager.setMonitorNotifier(new MonitorNotifier() {
+			@Override
+			public void didEnterRegion(Region region) {
+				Log.e("BeaconDetactorService", "didEnterRegion");
+				// logStatus("I just saw an iBeacon for the first time!");
+			}
+
+			@Override
+			public void didExitRegion(Region region) {
+				Log.e("BeaconDetactorService", "didExitRegion");
+				// logStatus("I no longer see an iBeacon");
+			}
+
+			@Override
+			public void didDetermineStateForRegion(int state, Region region) {
+				Log.e("BeaconDetactorService", "didDetermineStateForRegion");
+				// logStatus("I have just switched from seeing/not seeing iBeacons: " + state);
+			}
+
+		});
+
+		try {
+			iBeaconManager.startRangingBeaconsInRegion(new Region("myRangingUniqueId", null, null, null));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+
+		try {
+			iBeaconManager.startMonitoringBeaconsInRegion(new Region("myMonitoringUniqueId", null, null, null));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private class BeaconAdapter extends BaseAdapter {
+
+		@Override
+		public int getCount() {
+			if (arrayL != null && arrayL.size() > 0)
+				return arrayL.size();
+			else
+				return 0;
+		}
+
+		@Override
+		public IBeacon getItem(int arg0) {
+			return arrayL.get(arg0);
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return arg0;
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			try {
+				ViewHolder holder;
+
+				if (convertView != null) {
+					holder = (ViewHolder) convertView.getTag();
+				} else {
+					holder = new ViewHolder(convertView = inflater.inflate(R.layout.tupple_monitoring, null));
+				}
+				if (arrayL.get(position).getProximityUuid() != null)
+		//		holder.beacon_uuid.setText("UUID: " + arrayL.get(position).getProximityUuid());
+
+				holder.beacon_minor.setText("Key : " + arrayL.get(position).getMajor() + "-" + arrayL.get(position).getMinor());
+				
+				holder.beacon_range.setText("°Å¸® : " + Double.parseDouble(String.format("%.3f", arrayL.get(position).getAccuracy())) + " m");
+
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
+			return convertView;
+		}
+
+		private class ViewHolder {
+	//		private TextView beacon_uuid;
+	//		private TextView beacon_major;
+			private TextView beacon_minor;
+			private TextView beacon_range;
+
+			public ViewHolder(View view) {
+	//			beacon_uuid = (TextView) view.findViewById(R.id.BEACON_uuid);
+	//			beacon_major = (TextView) view.findViewById(R.id.BEACON_major);
+				beacon_minor = (TextView) view.findViewById(R.id.BEACON_minor);
+				beacon_range = (TextView) view.findViewById(R.id.BEACON_range);
+
+				view.setTag(this);
+			}
+		}
+
+	}
+
+}
